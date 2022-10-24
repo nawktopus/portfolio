24a988d77a76fb27ba25c8530a3916fd1c19a9d7
Jordan <jkwan104@gmail.com>
2022-10-23 23:09:46 -0700

    icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -31,7 +31,6 @@ class Footer extends Component {
               >
                 <span
                   className="iconify language-icon mr-5"
-                  data-icon="twemoji-flag-for-flag-united-kingdom"
                   data-inline="false"
                   id={window.$primaryLanguageIconId}
                 ></span>
@@ -47,7 +46,6 @@ class Footer extends Component {
               >
                 <span
                   className="iconify language-icon"
-                  data-icon="twemoji-flag-for-flag-poland"
                   data-inline="false"
                   id={window.$secondaryLanguageIconId}
                 ></span>
